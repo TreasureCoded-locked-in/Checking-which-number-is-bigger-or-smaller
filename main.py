f3c59948569a0e8255d30b833c3abe347a9a24f6
TreasureCoded-locked-in <akpannsikak410@gmail.com>
2024-10-26 16:47:57 +0100

    First Comment

---
+++ main.py
@@ -0,0 +1,6 @@
+i = int(input('enter a number: '))
+if (i < 15):
+    print ('i is smaller than 15')
+    print ('i am the if block')
+else: print('i is greater than 15')
+print('i am in the else block')
